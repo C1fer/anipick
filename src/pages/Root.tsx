465f512cb97ba57740af3592363d965a61610f1c
C1fer <rusbelduarte@gmail.com>
2026-03-07 14:55:02 -0400

    feat: update Root page style

--- src/pages/Root.tsx
+++ src/pages/Root.tsx
@@ -2,9 +2,9 @@ import { Outlet } from "react-router"
 import { TopBar } from "../components/TopBar"
 
 export const Root = () => (
-    <div className="flex flex-col bg-background min-h-screen">
+    <div className="flex flex-col bg-background h-screen">
         {/* <TopBar /> */}
-        <div className="">
+        <div className="flex-1 overflow-auto">
             <Outlet />
         </div>
     </div>
