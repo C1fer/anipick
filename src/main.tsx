ff1d13d9516a9564ada1a035f86202a3364c7b48
C1fer <rusbelduarte@gmail.com>
2026-03-21 19:09:07 -0400

    fix: mochiy font import

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import '@fontsource/mochiy-pop-one';
+import '@fontsource/mochiy-pop-one/400.css'
 
 import { Toaster } from 'sonner';
 import { AppRouter } from './pages/router';
